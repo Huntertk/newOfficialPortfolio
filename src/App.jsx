71730931a4fea138fefaa350f77cfa3699b2855e
MD TAUFIK <110840076+Huntertk@users.noreply.github.com>
2023-09-13 23:03:47 +0530

    In this commit adding profile page and in the profile page adding information of developer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import { Toaster } from 'react-hot-toast'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
       <Routes>
         <Route element={<Layout />}>
           <Route path='/' element={<Home />} />
-          <Route path='/profile' element={<h1>Profile</h1>} />
+          <Route path='/profile' element={<Profile />} />
           <Route path='/projects' element={<h1>Projects</h1>} />
           <Route path='/contact' element={<h1>Contact</h1>} />
         </Route>
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/porfile.scss'
+import DeveloperDetails from '../components/DeveloperDetails'
+
+const Profile = () => {
+  return (
+    <section className='profileMainContainer'>
+        <DeveloperDetails />
+    </section>
+  )
+}
+
+export default Profile

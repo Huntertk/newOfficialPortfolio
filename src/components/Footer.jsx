e65d06bc2b06f966894d70c0a0b8be80c04cbad8
MD TAUFIK <110840076+Huntertk@users.noreply.github.com>
2023-09-13 19:05:09 +0530

    In this commit Footer are created and optimising for every device

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import '../styles/footer.scss'
+import {BsFillSuitHeartFill,BsGithub,BsLinkedin,BsInstagram} from 'react-icons/bs';
+import {motion} from 'framer-motion';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className='footer'>
+      <div className="footerWrapperContainer">
+        <div className="footerMainContainer">
+          <motion.div className="iconContainer"
+          initial={{
+            x:"-100%",
+            opacity:0
+          }}
+
+          whileInView={{
+            x:0,
+            opacity:1
+          }}
+          >
+            <BsGithub />  
+            <BsLinkedin />
+            <BsInstagram />
+          </motion.div>
+          <motion.div
+            initial={{
+            scale:0.1,
+            opacity:0
+          }}
+
+          whileInView={{
+            scale:1,
+            opacity:1
+          }}
+          className="devDetails">
+            <p>Created With <BsFillSuitHeartFill /> by</p>
+            <p className='devName'>MD TAUFIK</p>
+          </motion.div>
+        </div>
+      </div>
+    </section>
   )
 }
 

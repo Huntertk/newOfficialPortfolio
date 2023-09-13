46df9b2d19a1a47e8449a17bd89fe6a0d1ee7ad2
MD TAUFIK <110840076+Huntertk@users.noreply.github.com>
2023-09-13 12:53:46 +0530

    In this commit the skills componed added to the home and making it responsive for all device

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,78 +1,16 @@
 import React from 'react'
-import {motion} from 'framer-motion'
-import avatar from '../assets/taufik.png'
 import '../styles/home.scss'
+import Skills from './Skills'
+import HomeIntroContainer from '../components/HomeIntroContainer'
 
 
 
 const Home = () => {
-  const easeScaleUp = {
-    initial:{
-      scale:0.1,
-      opacity:0
-    },
-    whileInView:{
-      scale:1,
-      opacity: 1
-    }
-  }
-
-    const easeMoveTop = {
-    initial:{
-      y:"100%",
-      opacity:0
-    },
-    whileInView:{
-      y:0,
-      opacity: 1
-    }
-  }
 
   return (
     <section className='homeMainContainer'>
-      <div className="topmainContainer">
-        <div className="textContainer">
-          <motion.h1 {...easeScaleUp}>Hey, I am MD TAUFIK</motion.h1>
-           <motion.img 
-          initial={{
-            opacity:0
-          }}
-          whileInView={{
-            opacity:1
-          }}
-          src={avatar} alt="" />
-          <motion.p {...easeMoveTop}>I am front end developer build modern and responsive website using various technology like reactjs and nextjs</motion.p>
-         
-          <motion.button {...easeMoveTop} className='btn'>Let's Connect</motion.button>
-        </div>
-
-        <div className="homeBottomContainer">
-          <div className="modalMainContainer">
-            <div className="modalTopContainer">
-              <div className="dot"></div>
-              <div className="dot"></div>
-              <div className="dot"></div>
-            </div>
-            <div className="modalBottomContainer">
-              <motion.h1 {...easeScaleUp}>An Introduction</motion.h1>
-              <motion.p {...easeMoveTop}>I am a skilled Front End Developer with over 6 Months of
-                experience in creating responsive and accessible web
-                applications. <br /><br />My expertise includes HTML, CSS,
-                JavaScript and Advance React framework. I have
-                ability to create Modern Responsive UI with Animation.
-                As a developer, I am passionate about creating
-                intuitive and user-friendly web interfaces that deliver a
-                seamless user experience. <br /><br />I am always eager to learn
-                new technologies and techniques to enhance my skills
-                and stay up-to-date with industry trends. My attention
-                to detail, strong problem-solving skills, and ability to
-                work well in a team make me an ideal candidate for
-                any Front End Developer role.
-              </motion.p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <HomeIntroContainer />
+      <Skills />
     </section>
   )
 }
---
+++ src/pages/Skills.jsx
@@ -0,0 +1,169 @@
+import React from 'react'
+import {FaGreaterThan} from 'react-icons/fa'
+import {FaLessThan} from 'react-icons/fa'
+import '../styles/skills.scss'
+import {motion} from 'framer-motion'
+
+import {
+    BiLogoJavascript, 
+    BiLogoHtml5,
+    BiLogoCss3,
+    BiLogoReact,
+    BiLogoMongodb,
+    BiLogoNetlify,
+    BiLogoGit
+} from 'react-icons/bi'
+import {
+    FaSass,
+    FaBootstrap,
+    FaNode,
+    FaGithub
+} from 'react-icons/fa'
+
+import {SiExpress} from 'react-icons/si'
+
+const Skills = () => {
+
+  const easingItemTop={
+    initial:{
+      y:"-100%",
+      opacity:0
+    },
+    whileInView:{
+      y:0,
+      opacity:1
+    },
+    transition:{duration:0.5}
+  }
+
+  const easingItemBottom={
+    initial:{
+      y:"100%",
+      opacity:0
+    },
+    whileInView:{
+      y:0,
+      opacity:1
+    },
+    transition:{duration:0.5}
+  }
+
+  const easingItemRight={
+    initial:{
+      x:"100%",
+      opacity:0
+    },
+    whileInView:{
+      x:0,
+      opacity:1
+    },
+    transition:{duration:0.5}
+  }
+
+  const easingItemLeft={
+    initial:{
+      x:"-100%",
+      opacity:0
+    },
+    whileInView:{
+      x:0,
+      opacity:1
+    },
+    transition:{duration:0.5}
+  }
+
+
+
+
+  return (
+    <section className='skillsMainContainer'>
+      <h1><FaLessThan/><span>Skills /</span><FaGreaterThan/></h1>
+      <div className="skillsIconsMainContainer">
+        <motion.div 
+        {...easingItemLeft}
+        className="iconContainer"
+        >
+          <BiLogoReact />
+          <span>React Js</span>
+        </motion.div>
+
+         <motion.div 
+        {...easingItemRight} className="iconContainer">
+          <SiExpress />
+          <span>Express Js</span>
+        </motion.div>
+
+        <motion.div 
+        {...easingItemRight} className="iconContainer">
+          <FaNode />
+          <span>Node Js</span>
+        </motion.div>
+
+        <motion.div 
+        {...easingItemLeft} className="iconContainer">
+          <BiLogoMongodb />
+          <span>MongoDB</span>
+        </motion.div>
+
+        <motion.div 
+        {...easingItemLeft}
+        className="iconContainer">
+          <BiLogoJavascript />
+          <span>JavaScript</span>
+        </motion.div>
+
+        <motion.div 
+        {...easingItemRight} 
+        className="iconContainer">
+          <FaSass />
+          <span>Sass</span>
+        </motion.div>
+        
+
+        <motion.div 
+        {...easingItemRight}  
+        className="iconContainer">
+          <FaBootstrap />
+          <span>Bootstrap</span>
+        </motion.div>
+
+        <motion.div 
+        {...easingItemLeft} 
+         className="iconContainer">
+          <BiLogoHtml5 />
+          <span>HTML</span>
+        </motion.div>
+
+        <motion.div 
+        {...easingItemRight}  
+        className="iconContainer">
+          <BiLogoCss3 />
+          <span>CSS</span>
+        </motion.div>
+
+        <motion.div 
+        {...easingItemLeft} 
+         className="iconContainer">
+          <FaGithub />
+          <span>GitHub</span>
+        </motion.div>
+
+        <motion.div 
+        {...easingItemRight}  
+        className="iconContainer">
+          <BiLogoGit />
+          <span>Git</span>
+        </motion.div>
+
+        <motion.div 
+        {...easingItemLeft} 
+         className="iconContainer">
+          <BiLogoNetlify />
+          <span>Netlify</span>
+        </motion.div>
+      </div>
+    </section>
+  )
+}
+
+export default Skills
